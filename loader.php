b49009f65ca3e8fd3b34d9c91cfbd8de39b93f92
Sentialcc <106581565+Sentialcc@users.noreply.github.com>
2022-06-06 20:35:56 -0400

    Update loader.php

--- loader.php
+++ loader.php
@@ -1,1 +1,2 @@
-game.Players.LocalPlayer:Kick('The owner is a scammer, script has been taken down')
+setclipboard('https://v3rmillion.net/showthread.php?tid=1172901')
+game.Players.LocalPlayer:Kick('The owner is a scammer, script has been taken down | Script has copied CW to your clipboard.')
